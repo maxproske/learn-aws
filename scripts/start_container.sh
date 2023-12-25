d51e1ca00375d80549b2b12b4702c345bc5bf9dd
Max Proske <max@mproske.com>
2023-12-24 17:59:15 -0800

    Update start/stop script

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull maxproske/simple-python-flask-app
+sudo docker pull maxproske/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 maxproske/simple-python-flask-app
+sudo docker run -d -p 5000:5000 maxproske/simple-python-flask-app
