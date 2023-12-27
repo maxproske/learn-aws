0af71df88d6c09a91092638912f43120d83b2f0b
Max Proske <max@mproske.com>
2023-12-26 17:34:48 -0800

    Start defining S3 plan

--- 24-terraform/main.tf
+++ 24-terraform/main.tf
@@ -14,7 +14,7 @@ resource "aws_subnet" "mysub1" {
   cidr_block = "10.0.0.0/24"
   // Availability Zone A
   availability_zone = "us-east-1a"
-  // Assign a public IPv4
+  // Assign a public IPv4 - Not enabled by default!
   map_public_ip_on_launch = true
 }
 
@@ -55,4 +55,53 @@ resource "aws_route_table_association" "myrta1" {
 resource "aws_route_table_association" "myrta2" {
   subnet_id      = aws_subnet.mysub2.id
   route_table_id = aws_route_table.myrt.id
+}
+
+// Security Group
+// https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group
+resource "aws_security_group" "mysg" {
+  name        = "mysg"
+  vpc_id      = aws_vpc.myvpc.id
+
+  ingress {
+    description      = "TCP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+// Don't need this, since security groups automatically open inbound ports for outbound traffic
+#   egress {
+#     from_port        = 0
+#     to_port          = 0
+#     protocol         = "-1"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
+
+  // Optionally, name the resource
+  tags = {
+    Name = "mysg"
+  }
+}
+
+// S3 Bucket
+// https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+resource "aws_s3_bucket" "mys3" {
+  // Globally unique name
+  bucket = "maxproske-learn-terraform-s3-bucket"
+
+  // Access Control List is deprecated
+  # acl    = "public-read"
+
+  
 }
